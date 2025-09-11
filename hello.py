12995631468a30cfdca9b092c37aaabee40a4d6e
GavishKapoor <gavishkapoor64@gmail.com>
2025-09-11 11:28:02 +0530

    First working version of disease prediction app

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Flask is working!"
+def hello():
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
